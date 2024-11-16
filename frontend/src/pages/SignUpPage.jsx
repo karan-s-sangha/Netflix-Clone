03a3383180a5f4936b199210cd451c5e3a8c3666
karan <karandeepsangha95@gmail.com>
2024-11-16 14:32:44 -0800

    loading states added

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
-import { useAuthStore } from "../store/authUser.js";
+import { useAuthStore } from "../store/authUser";
 
 const SignUpPage = () => {
 	const { searchParams } = new URL(document.location);
