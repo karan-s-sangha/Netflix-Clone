d7682b1db13d1e0a720d278f584df0c0721cfb58
karan <karandeepsangha95@gmail.com>
2024-11-16 13:35:28 -0800

    Added Search History

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,7 @@ import SignUpPage from "./pages/SignUpPage";
 import Footer from "./components/Footer";
 import WatchPage from "./pages/WatchPage";
 import SearchPage from "./pages/SearchPage";
+import SearchHistoryPage from "./pages/SearchHistoryPage";
 import { Toaster } from "react-hot-toast";
 import { useAuthStore } from "./store/authUser";
 import { useEffect } from "react";
@@ -35,7 +36,8 @@ function App() {
 				<Route path='/signup' element={!user ? <SignUpPage /> : <Navigate to={"/"} />} />
 				<Route path='/watch/:id' element={user ? <WatchPage /> : <Navigate to={"/login"} />} />
 				<Route path='/search' element={user ? <SearchPage /> : <Navigate to={"/login"} />} />
-				<Route path='/history' element={user ? <HistoryPage /> : <Navigate to={"/login"} />} />
+				<Route path='/history' element={user ? <SearchHistoryPage /> : <Navigate to={"/login"} />} />
+				<Route path='/*' element={<NotFoundPage />} />
 			</Routes>
 			<Footer />
 
