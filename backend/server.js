ab3d83e548fcdc564aed9c8b347dc7097ae1d233
karan <karandeepsangha95@gmail.com>
2026-03-31 22:02:37 -0700

    fix Express 5 no longer supports the bare * wildcard bug

--- backend/server.js
+++ backend/server.js
@@ -48,7 +48,7 @@ app.get('/ping', (req, res) => {
 
 if (ENV_VARS.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "/frontend/dist")));
-    app.get("*", (req, res) => {
+    app.get("/{*path}", (req, res) => {
         res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
     });
 
